bba43865cf606f99a1f8a7f44faeb013581267aa
Johnny Choo <jchoo156@gmail.com>
2016-11-12 15:46:17 -0800

    add styling to show page

--- src/Show.js
+++ src/Show.js
@@ -3,7 +3,24 @@ const React = require('react')
 const Show = React.createClass({
   render(){
     return (
-      <h1>Show</h1>
+      <div className='showbg'>
+        <h1>Experiment Show</h1>
+        <div className='img-border'>
+          <div className='img'></div>
+          <div className='img-description'>
+            <div className='descriptionh2'>
+              <h2>Description</h2>
+            </div>
+            <p> aidsjfalsdjf </p>
+            <p> aidsjfalsdjf </p>
+            <p> aidsjfalsdjf </p>
+            <p> aidsjfalsdjf </p>
+            <p> aidsjfalsdjf </p>
+          </div>
+        </div>
+
+        <h3 className='comments'>Comments</h3>
+      </div>
     )
   }
 })
