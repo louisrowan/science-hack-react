cd1f5cbf3d1b7f4b03ece62e7afb4dea92e8b4f7
Kim Stephenson <kmstephe@usc.edu>
2016-11-12 20:17:00 -0800

    Merge master

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <a href='/#/show'>Show</a><br />
-      <a href='/#/search'>Search</a>
-        {this.props.children}
+        <a href='/#/show'>Show</a><br />
+        <a href='/#/search'>Search</a>
+          {this.props.children}
       </div>
     );
   }
