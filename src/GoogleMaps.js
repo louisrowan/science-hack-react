044e22abd4092f1b80564c89e81121de0131574e
Johnny Choo <jchoo156@gmail.com>
2016-11-12 20:26:33 -0800

    add google maps file

---
+++ src/GoogleMaps.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Gmaps, Marker, InfoWindow, Circle} from 'react-gmaps';
+
+const coords = {
+  lat: 51.5258541,
+  lng: -0.08040660000006028
+};
+
+const App = React.createClass({
+
+  onMapCreated(map) {
+    map.setOptions({
+      disableDefaultUI: true
+    });
+  },
+
+  onDragEnd(e) {
+    console.log('onDragEnd', e);
+  },
+
+  onCloseClick() {
+    console.log('onCloseClick');
+  },
+
+  onClick(e) {
+    console.log('onClick', e);
+  },
+
+  render() {
+    return (
+      <Gmaps
+        width={'800px'}
+        height={'600px'}
+        lat={coords.lat}
+        lng={coords.lng}
+        zoom={12}
+        loadingMessage={'Be happy'}
+        params={{v: '3.exp', key: 'YOUR_API_KEY'}}
+        onMapCreated={this.onMapCreated}>
+        <Marker
+          lat={coords.lat}
+          lng={coords.lng}
+          draggable={true}
+          onDragEnd={this.onDragEnd} />
+        <InfoWindow
+          lat={coords.lat}
+          lng={coords.lng}
+          content={'Hello, React :)'}
+          onCloseClick={this.onCloseClick} />
+        <Circle
+          lat={coords.lat}
+          lng={coords.lng}
+          radius={500}
+          onClick={this.onClick} />
+      </Gmaps>
+    );
+  }
+
+});
+
+ReactDOM.render(<App />, document.getElementById('gmaps'));
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 const Show = require('./Show')
 const Search = require('./Search')
+const GoogleMaps = require('./GoogleMaps')
 import './index.css';
 const { Router, Route, hashHistory } = require('react-router')
 
@@ -11,6 +12,7 @@ const Index = () => (
     <Route path='/' component={App}>
       <Route path='/search' component={Search} />
       <Route path='/show/:id' component={Show} />
+      <Route path='/googleMaps' component={GoogleMaps} />
     </Route>
   </Router>
 )
