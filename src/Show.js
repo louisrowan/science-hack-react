e19d87413ba6fce733c733618bbbbd12f2ca12be
Louis Rowan <louis.rowan@icloud.com>
2016-11-13 01:31:23 -0800

    adds ajax to comment, adds list appearance for procedure

--- src/Show.js
+++ src/Show.js
@@ -6,7 +6,9 @@ const Show = React.createClass({
   getInitialState: function(){
     return {
       data: '',
-      materials: ''
+      materials: '',
+      comment: '',
+      enteredComments: ''
     }
   },
   componentWillMount: function(){
@@ -33,13 +35,31 @@ const Show = React.createClass({
       that.setState({ materials: materials })
     })
   },
+  formChange: function(e){
+    this.setState({ comment: e.target.value })
+  },
+  formSubmit: function(){
+    const appendText = `<div className='comment'><p>${this.state.comment}</p><h6>5 hrs ago</h6>`
+    this.setState({ enteredComments: this.state.enteredComments += appendText, comment: ''})
+  },
+  createMarkup: function(){
+    return {__html: this.state.enteredComments}
+  },
   render(){
-    console.log(this.state)
     let materialData
     if (this.state.materials) {
       materialData = this.state.materials.map((m, i) => (<div className='materials-div-double' key={i}><div className='materials-img-div'><img src={m.info} /></div><div className='materials-div-name'>{m.name}</div></div>))
     } else {
       materialData = ''
+    }
+
+    let procedureData
+    if (this.state.data.procedure){
+      const split = this.state.data.procedure.split(',')
+      console.log(split)
+      procedureData = split.map((p, i) => (
+        <li key={i}>{p.replace(/["[\]]/g, '')}</li>
+      ))
     }
     return (
       <div className='showbg'>
@@ -53,9 +73,9 @@ const Show = React.createClass({
         </div>
         <div className='show-procedure'>
           <h2>Procedure</h2>
-          <ul>
-          {this.state.data.procedure}
-          </ul>
+          <ol className='procedure-list'>
+          {procedureData}
+          </ol>
         </div>
 
         <h4 className='commentsh3'>Comments</h4>
@@ -72,10 +92,11 @@ const Show = React.createClass({
             <p>The intricacy behind the creation of this experiment is absolutely astounding.  Keep up the good work bra.</p>
             <h6>5 hrs ago</h6>
           </div>
+          <div dangerouslySetInnerHTML={this.createMarkup()}></div>
         </div>
-        <form action="demo_form.asp">
+        <form onSubmit={this.formSubmit}>
           <h5>Comment</h5>
-          <textarea rows="4" cols="50" /><br />
+          <textarea rows="4" cols="50" value={this.state.comment} onChange={this.formChange} /><br />
           <input type="submit" value="Submit" />
         </form>
       </div>
