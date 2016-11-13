0daf59e83140f7006ea58ca251cee7ba1377a5ed
Johnny Choo <jchoo156@gmail.com>
2016-11-12 16:40:12 -0800

    add comments section to show route

--- src/Show.js
+++ src/Show.js
@@ -19,7 +19,21 @@ const Show = React.createClass({
           </div>
         </div>
 
-        <h3 className='comments'>Comments</h3>
+        <h3 className='commentsh3'>Comments</h3>
+        <div className='comments'>
+          <div className='comment'>
+            <p>This is an amazing experiment!</p>
+            <h6>Post: 3 hrs ago</h6>
+          </div>
+          <div className='comment'>
+            <p>How did you do this?</p>
+            <h6>Post: 5 hrs ago</h6>
+          </div>
+          <div className='comment'>
+            <p>The intricacy behind the creation of this experiment is absolutely astounding.  Keep up the good work bra.</p>
+            <h6>Post: 5 hrs ago</h6>
+          </div>
+        </div>
       </div>
     )
   }
