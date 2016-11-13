32d3f319a96ba2903c8cd7910288b5c1ecd3bbe8
Louis Rowan <louis.rowan@icloud.com>
2016-11-12 20:18:13 -0800

    shows pictures on show page

--- src/Show.js
+++ src/Show.js
@@ -4,35 +4,56 @@ import $ from 'jquery'
 const Show = React.createClass({
   getInitialState: function(){
     return {
-      data: ''
+      data: '',
+      materials: ''
     }
   },
   componentWillMount: function(){
     var that = this
     var path = this.props.location.pathname
+    path = path.slice(-1)
     var request = $.ajax({
       url: 'http://rainydayscience.herokuapp.com/',
       dataType: 'json'
     })
      request.done(function(r){
-      path = path.slice(-1)
       path = parseInt(path)
       const data = r.filter((d) => (d.id === path))
       that.setState({ data: data[0] })
     })
+    var materialRequest = $.ajax({
+      url: 'http://rainydayscience.herokuapp.com/static_pages/material',
+      dataType: 'json'
+    })
+    materialRequest.done(function(r){
+      console.log(r)
+      path = parseInt(path)
+      const materials = r.filter((d) => (d.experiment_id == path))
+      that.setState({ materials: materials })
+    })
   },
   render(){
-    console.log(this.state.data)
+    console.log(this.state)
+    let materialData
+    if (this.state.materials) {
+      materialData = this.state.materials.map((m, i) => (<li key={i}>{m.name}</li>))
+    } else {
+      materialData = ''
+    }
     return (
       <div className='showbg'>
         <div className='img-border'>
-          <div className='img'></div>
+          <div className='img'><img src={this.state.data.picture} /></div>
           <div className='img-description'>
             <div className='descriptionh2'>
-              <h2>Description</h2>
-              <p><b>Rating</b>: 9.4/10</p>
+              <h2>{this.state.data.name}</h2>
+              <p><b>Rating</b>: {this.state.data.rating}</p>
             </div>
-            <p>The tag defines a multi-line text input control.  A text area can hold an unlimited number of characters, and the text renders in a fixed-width font (usually Courier).  The size of a text area can be specified by the cols and rows attributes, or even better; through CSS' height and width properties. </p>
+            <p>{this.state.data.description}</p>
+            <h2>Materials</h2>
+            <ul>
+              {materialData}
+            </ul>
           </div>
         </div>
 
