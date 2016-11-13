823c31e5d64d0196d6ea8bb75d9ef056c0889458
Kim Stephenson <kmstephe@usc.edu>
2016-11-12 22:36:06 -0800

    Start styling homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
+import splashImage from '../public/kids-doing-science.jpg'
 
 const App = React.createClass({
   render() {
     return (
       <div className="App">
-         <ul id="navbar">
+        <ul id="navbar">
+         <li><a href='/'>Home</a></li>
           <li><a href='/#/search'>Search</a></li>
-          <li><a href='/#/show'>Show</a></li>
         </ul>
+
+        <div className="splash">
+          <img id="splash-image" src={splashImage} alt="kids doing experiment"/>
+          <h1 id="splash-header">Rainy Day Science</h1>
+        </div>
         {this.props.children}
       </div>
     );
